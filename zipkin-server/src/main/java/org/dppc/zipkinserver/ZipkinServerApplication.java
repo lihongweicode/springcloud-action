6a7eea96a2f7ce9cd011ba2b9ec6a31c3d4bf627
lihongweicode <1820994758@qq.com>
2018-12-27 16:16:52 +0800

    first commit

---
+++ zipkin-server/src/main/java/org/dppc/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,19 @@
+package org.dppc.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+
+@EnableZipkinServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+}
+
